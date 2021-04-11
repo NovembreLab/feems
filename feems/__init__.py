dffdfe1af23fec77888a5cb6fe63320ec4c8bf89
jhmarcus <jhmarcus@uchicago.edu>
2021-04-11 16:11:14 -0700

    cleaned up cv curve and start cv notebook

--- feems/__init__.py
+++ feems/__init__.py
@@ -1,9 +1,8 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
+from .cv import run_cv
+from .objective import Objective, loss_wrapper, neg_log_lik_w0_s2
 from .spatial_graph import SpatialGraph, query_node_attributes
-from .objective import Objective, neg_log_lik_w0_s2, loss_wrapper
 from .viz import Viz
 
 __version__ = "1.0.0"
