0c3c2ceab3f13c6edf449564da1874a4e813b8a3
VivaswatS <vivshastry@gmail.com>
2021-07-07 11:47:33 -0500

    algorithms for exact and heuristic search (default + uneven sampling sims)

--- feems/sim.py
+++ feems/sim.py
@@ -188,6 +188,7 @@ def setup_graph_long_range(
     -------
     tuple of graph objects
     """
+    assert type(long_range_nodes) == list, "long_range_nodes must be a list of 2-tuples"
     assert len(long_range_edges) == len(long_range_nodes), "equal number of pairs of nodes and corresponding edges for long range"
 
     graph = nx.generators.lattice.triangular_lattice_graph(
@@ -213,10 +214,22 @@ def setup_graph_long_range(
                 n_samples_per_node, barrier_prob
             )
 
-        # sample a node or not
-        graph.nodes[i]["sample_size"] = graph.nodes[i][
-            "sample_size"
-        ] * np.random.binomial(1, sample_prob)
+        # sample a node or not (leave long_range_nodes as is, hard to pick another set)
+        if i not in np.ravel(long_range_nodes):
+            graph.nodes[i]["sample_size"] = graph.nodes[i]["sample_size"] * np.random.binomial(1, sample_prob) 
+
+    # # add check to ensure that the requested long_range_nodes have been sampled
+    # # if not, assign to a new pair of nodes in the vicinity 
+    # zero_nodes = [(idxz, val) for idxz, val in enumerate(list(np.ravel(long_range_nodes))) if graph.nodes[val]["sample_size"]==0]
+    # if len(zero_nodes)!=0:
+    #     for vz in zero_nodes:
+    #         for neighbor in graph.neighbors(vz[1]):
+    #             if(graph.nodes[neighbor]["sample_size"]!=0):
+    #                 # break out of for loop after finding nearest neighbor
+    #                 print("Node %d not sampled, replaced with Node %d"%(vz[1],neighbor))
+    #                 idx = np.floor(vz[0]/2).astype("int")
+    #                 long_range_nodes[idx] = (neighbor,long_range_nodes[idx][1]) if vz[0]%2==0 else (long_range_nodes[idx][0],neighbor)
+    #                 break
 
     # assign edge weights
     for i, j in graph.edges():
